b8c27fde4fe07c7f8fa835ae24d0166bf7182416
Bernovich Denis <denisbernovich@gmail.com>
2023-08-07 18:46:18 +0300

    fix: burger menu 2

--- library/scripts/showMenu.js
+++ library/scripts/showMenu.js
@@ -2,25 +2,24 @@ export const showMenu = () => {
   const burgerButton = document.querySelector('.burger-menu-button');
   const burgerLinks = document.querySelectorAll('.burger-link');
   const burgerMenu = document.querySelector('.burger-links');
-  const activeBlackout = document.querySelector('.burger-menu');
 
-  burgerButton.addEventListener('click', () => {
-    activeBlackout.classList.toggle('active-blackout');
+  const toggleClass = () => {
     burgerButton.classList.toggle('active-button');
     burgerMenu.classList.toggle('active-menu');
+  }
+
+  burgerButton.addEventListener('click', () => {
+    toggleClass();
   });
 
   burgerLinks.forEach(link => link.addEventListener('click', () => {
-    burgerButton.classList.toggle('active-button');
-    burgerMenu.classList.toggle('active-menu');
-    activeBlackout.classList.toggle('active-blackout');
+    toggleClass();
   }));
 
-  activeBlackout.addEventListener('click', (e) => {
-    if (e.target === activeBlackout) {
+  window.addEventListener('click', (event) => {
+    if (!burgerMenu.contains(event.target) && !burgerButton.contains(event.target)) {
       burgerButton.classList.remove('active-button');
       burgerMenu.classList.remove('active-menu');
-      activeBlackout.classList.toggle('active-blackout');
     }
-  })
+  });
 }
