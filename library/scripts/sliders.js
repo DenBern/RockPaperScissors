8aeaa07ba07ceeab2b2f167030088b6e3697556b
Denis <denisbernovich@gmail.com>
2023-08-21 12:54:25 +0300

    fix: slider buttons

--- library/scripts/sliders.js
+++ library/scripts/sliders.js
@@ -3,27 +3,29 @@ export const sliders = () => {
   const sliderBtns = document.querySelectorAll('.dot');
   const rightButton = document.querySelector('.button-right');
   const leftButton = document.querySelector('.button-left');
+  const leftSvg = document.getElementById('btn-left');
+  const rightSvg = document.getElementById('btn-right');
 
   // Variables
 
+  const sliderCenterPosition = 0;
   const sliderTabletStartPosition = 1016;
   const sliderTabletEndPosition = -1016;
   const sliderTabletStep = 508;
   const sliderDesktopStartPosition = 477;
   const sliderDesktopEndtPosition = -477;
-  const sliderCenterPosition = 0;
   let sliderPosition = parseInt(window.getComputedStyle(sliderImages).left);
   const screenWith = window.screen.width > 1024;
 
-  // Active buttons (dots)
+  // Active buttons(dots) desktop
 
   const setActiveBtn = (btnName) => {
     sliderBtns.forEach(btn => {
-        btn.classList.toggle('active', btn.classList.contains(btnName));
+      btn.classList.toggle('active', btn.classList.contains(btnName));
     })
   }
 
-// Set attribute btns (tablet)
+  // Set attribute btns(dots) tablet
 
   const setAttributeBtn = (btn) => {
     switch (btn) {
@@ -86,39 +88,60 @@ export const sliders = () => {
 
   // Disabled buttons(tablet)
 
-  const setDisabledBtn = (arg) => {
-    if (arg === sliderTabletStartPosition) {
+  const setDisabledBtn = (position) => {
+    if (position === sliderTabletStartPosition) {
       leftButton.setAttribute('disabled', '');
-    } else if (arg === sliderTabletEndPosition) {
+    } else if (position === sliderTabletEndPosition) {
       rightButton.setAttribute('disabled', '');
     }
   }
   setDisabledBtn(sliderPosition);
 
-  // Move slider buttons tablet
+  // Move slider buttons tablet & active buttons(dots) tablet
+
+  const activeBtnTablet = (position) => {
+    switch (position) {
+      case sliderTabletStartPosition:
+        setActiveBtn('first-dot');
+        break;
+      case sliderTabletStep:
+        setActiveBtn('second-dot');
+        break;
+      case sliderCenterPosition:
+        setActiveBtn('third-dot');
+        break;
+      case -sliderTabletStep:
+        setActiveBtn('fourth-dot');
+        break;
+      case sliderTabletEndPosition:
+        setActiveBtn('fifth-dot');
+        break;
+    }
+  }
 
   const shiftLeft = () => {
-      sliderPosition > sliderTabletEndPosition ? sliderPosition -= sliderTabletStep : 0;
-      if (sliderPosition === sliderTabletEndPosition) {
-          rightButton.setAttribute('disabled', '');
-          moveSlider(0, sliderPosition);
-      } else {
-          moveSlider(0, sliderPosition);
-          leftButton.removeAttribute('disabled', '');
-      }
-      setActiveBtn();
+    sliderPosition > sliderTabletEndPosition ? sliderPosition -= sliderTabletStep : 0;
+    if (sliderPosition === sliderTabletEndPosition) {
+      rightButton.setAttribute('disabled', '');
+      moveSlider(0, sliderPosition);
+    } else {
+      moveSlider(0, sliderPosition);
+      leftButton.removeAttribute('disabled', '');
+    }
+    activeBtnTablet(sliderPosition);
   }
   rightButton.addEventListener('click', (shiftLeft));
 
   const shiftRight = () => {
-      sliderPosition < sliderTabletStartPosition ? sliderPosition += sliderTabletStep : 0;
-      if (sliderPosition === sliderTabletStartPosition) {
-          leftButton.setAttribute('disabled', '');
-          moveSlider(0, sliderPosition);
-      } else {
-        moveSlider(0, sliderPosition);
-        rightButton.removeAttribute('disabled', '');
-      }
+    sliderPosition < sliderTabletStartPosition ? sliderPosition += sliderTabletStep : 0;
+    if (sliderPosition === sliderTabletStartPosition) {
+      leftButton.setAttribute('disabled', '');
+      moveSlider(0, sliderPosition);
+    } else {
+      moveSlider(0, sliderPosition);
+      rightButton.removeAttribute('disabled', '');
+    }
+    activeBtnTablet(sliderPosition);
   }
   leftButton.addEventListener('click', (shiftRight));
 }
