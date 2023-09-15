92771e91cce91f2c8ee6b45a0ae93bfe47cabc68
Denis <denisbernovich@gmail.com>
2023-09-15 21:46:51 +0300

    feat: add tracks

---
+++ js30#1.2-audio-player/data.js
@@ -0,0 +1,32 @@
+export const tracks = [
+  {
+    src: "./assets/audio/Alx Beats - Late Night Drive.mp3"
+  },
+  {
+    src: "./assets/audio/Alx Beats - Late Night Drive.mp3"
+  },
+  {
+    src: "./assets/audio/Alx Beats - Late Night Drive.mp3"
+  },
+  {
+    src: "./assets/audio/Alx Beats - Late Night Drive.mp3"
+  },
+  {
+    src: "./assets/audio/Alx Beats - Late Night Drive.mp3"
+  },
+  {
+    src: "./assets/audio/Alx Beats - Late Night Drive.mp3"
+  },
+  {
+    src: "./assets/audio/Alx Beats - Late Night Drive.mp3"
+  },
+  {
+    src: "./assets/audio/Alx Beats - Late Night Drive.mp3"
+  },
+  {
+    src: "./assets/audio/Alx Beats - Late Night Drive.mp3"
+  },
+  {
+    src: "./assets/audio/Alx Beats - Late Night Drive.mp3"
+  },
+]
