32b44a69a16f33602c9b806bffe029cf4fac97f2
Denis <denisbernovich@gmail.com>
2023-09-16 23:09:06 +0300

    feat: add functional

--- js30#1.2-audio-player/data.js
+++ js30#1.2-audio-player/data.js
@@ -1,50 +1,60 @@
 export const tracks = [
   {
+    cover: './assets/img/covers/Late_Night_Drive.jpg',
     track: "Late Night Drive",
     author: "Alx Beats",
     src: "./assets/audio/Alx Beats - Late Night Drive.mp3"
   },
   {
+    cover: './assets/img/covers/Sangria.jpg',
     track: "Sangria",
     author: "Alx Beats",
     src: "./assets/audio/Alx beats - Sangria.mp3"
   },
   {
+    cover: './assets/img/covers/Rave.jpg',
     track: "RAVE",
     author: "Dxrk",
     src: "./assets/audio/Dxrk - RAVE.mp3"
   },
   {
+    cover: './assets/img/covers/Twilight.jpg',
     track: "Twilight",
     author: "GraveChill",
     src: "./assets/audio/GraveChill - Twilight.mp3"
   },
   {
+    cover: './assets/img/covers/Dream.jpg',
     track: "DREAM",
     author: "HOSPICEMANE, purvs",
     src: "./assets/audio/HOSPICEMANE, purvs - DREAM.mp3"
   },
   {
+    cover: './assets/img/covers/Murder_In_My_Mind.jpg',
     track: "Murder In My Mind",
     author: "Kordhell",
     src: "./assets/audio/Kordhell - Murder In My Mind.mp3"
   },
   {
+    cover: './assets/img/covers/THIS_IS_LOVE.jpg',
     track: "THIS IS LOVE",
     author: "OBLXKQ",
     src: "./assets/audio/OBLXKQ - THIS IS LOVE.mp3"
   },
   {
+    cover: './assets/img/covers/ghost.jpg',
     track: "phonk.me, kiixshi",
     author: "ghost",
     src: "./assets/audio/phonk.me, kiixshi - ghost.mp3"
   },
   {
+    cover: './assets/img/covers/My_Idol.jpeg',
     track: "PLXYAE",
     author: "My Idol",
     src: "./assets/audio/PLXYAE - My Idol.mp3"
   },
   {
+    cover: './assets/img/covers/Unloved.jpg',
     track: "XVNVGE",
     author: "Unloved",
     src: "./assets/audio/XVNVGE - Unloved.mp3"
--- js30#1.2-audio-player/index.js
+++ js30#1.2-audio-player/index.js
@@ -4,14 +4,48 @@ const btnPlayTrack = document.querySelector('.play-track');
 const btnPauseTrack = document.querySelector('.pause-track');
 const btnNextTrack = document.querySelector('.next-track');
 const btnPrevTrack = document.querySelector('.prev-track');
+const author = document.querySelectorAll('.author');
+const currentTrack = document.querySelector('h2');
+const trackTime = document.querySelector('.track-time');
+const currentTrackTime = document.querySelector('.current-time');
+const cover = document.querySelector('.cover');
+const lineTime = document.querySelector('.line-time');
 
 const countTraks = tracks.length;
 let isPlay = false;
 let track;
+let durationtTrack = 0;
+let seconds = 0;
+let minutes = 0;
+let currentTime = 0;
+let currentMinutes = 0;
+let currentSeconds = 0;
+let timerId;
 
-window.onload = function() {
+window.onload = function () {
   track = 0;
+  changeVisual();
 }
+
+const changeVisual = () => {
+  author.forEach(item => item.textContent = `${tracks[track].author}`);
+  currentTrack.textContent = `${tracks[track].track}`;
+  trackTime.textContent = `${minutes ? minutes : '0'}:${seconds ? seconds : '00'}`;
+  currentTrackTime.textContent = '0:00';
+  cover.style.backgroundImage = `url(${tracks[track].cover})`;
+  lineTime.style.width = '0';
+}
+
+audio.load();
+
+audio.addEventListener('loadedmetadata', () => {
+  currentTime = Math.round(audio.currentTime);
+  durationtTrack = Math.round(audio.duration);
+  minutes = Math.trunc(durationtTrack / 60);
+  seconds = durationtTrack % 60;
+  changeVisual();
+});
+
 
 const toggleBtn = () => {
   if (isPlay) {
@@ -28,11 +62,26 @@ const setTrack = (track) => {
   audio.currentTime = 0;
 }
 
+
 const playTrack = () => {
+  clearInterval(timerId);
   isPlay = true;
   toggleBtn();
   setTrack(track);
   audio.play();
+  changeVisual();
+  timerId = setInterval(() => {
+      currentTime = Math.round(audio.currentTime);
+      currentMinutes = Math.trunc(currentTime / 60);
+      currentSeconds = currentTime % 60;
+    if (isPlay) {
+      lineTime.style.width = (currentTime * 100) / durationtTrack + '%';
+      currentTrackTime.textContent = `${currentMinutes + ':' + (currentSeconds < 10 ? '0' + currentSeconds : currentSeconds)}`;
+      if (durationtTrack === currentTime) {
+        nextTrack();
+      }
+    }
+  }, 1000)
 }
 
 const pauseTrack = () => {
@@ -55,5 +104,3 @@ btnPlayTrack.addEventListener('click', playTrack);
 btnPauseTrack.addEventListener('click', pauseTrack);
 btnNextTrack.addEventListener('click', nextTrack);
 btnPrevTrack.addEventListener('click', prevTrack);
-
-
