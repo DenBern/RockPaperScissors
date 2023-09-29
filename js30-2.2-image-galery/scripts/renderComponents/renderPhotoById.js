3f67f0a6c7786ae26fe2e93ab06f9102f181ee6b
Denis <denisbernovich@gmail.com>
2023-09-29 19:31:15 +0300

    fix: Remove unused fileы

--- js30-2.2-image-galery/scripts/renderComponents/renderPhotoById.js
+++
@@ -1,7 +0,0 @@
-import { fullSizePhoto, keywordDescription } from "../variables/variables.js";
-
-export const renderPhotoById = (photo) => {
-  fullSizePhoto.style.backgroundImage = `url(${photo.mediumSize})`;
-  fullSizePhoto.setAttribute('href', `${photo.originSize}`);
-  if (photo.description) keywordDescription.textContent = `${photo.description}`;
-};
