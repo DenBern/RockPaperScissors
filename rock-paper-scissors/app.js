3aac42acce63d3257947eab6c840aa74c1173eb7
Denis <denisbernovich@gmail.com>
2023-10-02 19:38:20 +0300

    feat: screen welcome

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,3 +1,4 @@
-import { screenGreeting } from "./screens/screenGreeting.js";
-
-screenGreeting();
+// import { screenGreeting } from "./screens/screenGreeting.js";
+import { screenWelcome } from "./screens/screenWelcome.js";
+// screenGreeting();
+screenWelcome();
